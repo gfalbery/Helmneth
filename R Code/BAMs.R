e8ffdfcee2e42f2f83bbf3925c2fede63a20d255
Greg Albery <gfalbery@gmail.com>
2019-03-04 23:43:08 -0500

    Prediction edits

--- R Code/BAMs.R
+++ R Code/BAMs.R
@@ -24,15 +24,18 @@ for(r in 1:length(WormGroups)){
 
 SpaceRange <- seq(from = min(FinalHostMatrix$Space),
                   to = max(FinalHostMatrix$Space),
-                  length = 100)
+                  length = 100) %>%
+  c(mean(FinalHostMatrix$Space))
 
 PhyloRange <- seq(from = min(FinalHostMatrix$Phylo),
                   to = max(FinalHostMatrix$Phylo),
-                  length = 100)
+                  length = 100) %>%
+  c(mean(FinalHostMatrix$Phylo))
 
 DietRange <- seq(from = min(FinalHostMatrix$DietSim),
                  to = max(FinalHostMatrix$DietSim),
-                 length = 100)
+                 length = 100) %>%
+  c(mean(FinalHostMatrix$DietSim))
 
 GAMPredDF <- expand.grid(Space = SpaceRange,
                          Phylo = PhyloRange,
@@ -54,23 +57,30 @@ ggplot(FitSlopeTime %>% filter(DietSim == 0), aes(Space, Phylo)) +
   labs(y = "Predicted Value") +
   facet_wrap(Eaten ~ Model, labeller = labeller(c("Not Eaten" = "0", "Eaten" = "1")))
 
-ggplot(FitSlopeTime, aes(Phylo, Estimate)) + 
-  geom_line(aes(colour = Space, group = as.factor(Space))) +
+ggplot(FitSlopeTime %>% 
+         filter(Space == dplyr::last(SpaceRange),
+                Phylo == dplyr::last(PhyloRange)), aes(DietSim, Estimate)) + 
+  geom_line(aes(colour = Eaten, group = as.factor(Eaten)), alpha = 0.5) +
   labs(y = "Predicted Value") +
-  facet_wrap(~Model)
+  facet_wrap(~ Model, labeller = labeller(Eaten = c("1" = "Eaten", "0" = "Not Eaten")), ncol = 2) +
+  ggsave("Figures/Helminth_Diet.jpeg", units = "mm", width = 150, height = 150)
 
-pdf("Tensor for CJC.pdf", height = 8, width = 20)
-
-ggplot(FitSlopeTime %>% filter(DietSim == 0), aes(Space, Estimate)) + 
+ggplot(FitSlopeTime %>% 
+         filter(DietSim == 0, Eaten == 1), aes(Space, Estimate)) + 
   geom_line(aes(colour = Phylo, group = as.factor(Phylo)), alpha = 0.5) +
   labs(y = "Predicted Value") +
-  facet_wrap(Eaten ~ Model, labeller = labeller(Eaten = c("1" = "Eaten", "0" = "Not Eaten")), ncol = 4)
+  facet_wrap(~ Model, labeller = labeller(Eaten = c("1" = "Eaten", "0" = "Not Eaten")), ncol = 2) +
+  ggsave("Figures/Helminth_Space.jpeg", units = "mm", width = 200, height = 200)
 
-ggplot(FitSlopeTime %>% filter(DietSim == 0), aes(Phylo, Estimate)) + 
+ggplot(FitSlopeTime %>% 
+         filter(DietSim == 0, Eaten == 1), aes(Phylo, Estimate)) + 
   geom_line(aes(colour = Space, group = as.factor(Space)), alpha = 0.5) +
   labs(y = "Predicted Value") +
-  facet_wrap(Eaten ~ Model, labeller = labeller(Eaten = c("1" = "Eaten", "0" = "Not Eaten")), ncol = 4)
+  facet_wrap(~ Model, labeller = labeller(Eaten = c("1" = "Eaten", "0" = "Not Eaten")), ncol = 2) +
+  ggsave("Figures/Helminth_Phylo.jpeg", units = "mm", width = 200, height = 200)
 
-dev.off()
+FitSlopeTime %>%
+  filter(DietSim = last(unique(DietSim)))
 
-
+BarGraph(FitSlopeTime, "Model", "Estimate", "Eaten") +
+  ggsave()
